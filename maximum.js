28fdf2deff74bf1af64eda48b77ffba3aed9acf1
bheem-works <shokokomichan098@gmail.com>
2025-06-10 20:20:29 +0545

    minimum value:

--- maximum.js
+++ maximum.js
@@ -10,3 +10,14 @@ for(let i = 0; i < value.length; i++) {
   }
 }
 console.log(maximum);
+
+// finding the smallest number 
+let minVal = [3,2,1,3,4,5];
+let minimum = value[0];
+
+for(let i = 0; i < minVal.length; i++) {
+  if(minVal[i] < minimum) { // Nothing is greater then the 1 that's why it just give the value; 
+  minimum = minVal[i];
+  }
+}
+console.log(minimum);
