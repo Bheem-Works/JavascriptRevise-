60e68daa80f69e928f0c026dbf7b6e07e1f7abf0
bheem-works <shokokomichan098@gmail.com>
2025-06-14 05:46:06 +0545

    codeWarsFourChoice

--- connectPlacingToken.js
+++ connectPlacingToken.js
@@ -1,21 +1,31 @@
+// Connects four placing tokens; 
+// Questions : A list of integers representing the columns (from 0 to 6) where tokens are dropped, in order. 
+// The first player is yellow (Y), followed by red (R), alternating turns.
 
-// find the out the grid by using the nested loops
+function moveFour(move){
+  let rows = 6;
+  let cols = 7;
+  let board = Array(rows).fill().map(()=>Array(cols).fill('-'))
 
-let rows = 6;
-let cols = 7;
-let boards = [];
+  for(let moveIdx = 0; moveIdx < move.length; moveIdx++){
+    let player = moveIdx % 2 === 0 ? "X" : "Y"
+    let col = move[moveIdx];
 
-
-for(let r = 0; r < rows; r++){
-  let row = [];
-  for(let c = 0; c < cols; c++){
-    row.push('-');
+    for(let row = rows-1; row >= 0; row--){
+      if(board[row][col] === '-'){
+        board[row][col] = player;
+        break;
+      }
+    }
   }
-    boards.push(row);
+  return board;
 }
 
-// output
-for(let r = 0; r < rows; r++){
-  console.log(boards[r])
+function printBoard(board){
+  for(let output of board){
+    console.log(output);
+  }
 }
 
+const moveOne = [0,1,2,4,5];
+console.log(moveFour(moveOne));
