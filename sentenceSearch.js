db3a5a0df584de3445fb2c41407470232caf8a33
bheem-works <shokokomichan098@gmail.com>
2025-06-18 14:09:46 +0545

    codeWarsProblems find the sentences index numbers

---
+++ sentenceSearch.js
@@ -0,0 +1,16 @@
+// This is the kata 6 problems; 
+// The output should be like this. 
+// [a-z] = a:1, b:2; 
+// [A-Z] = A:1, B: 2; 
+// [1-9] = 1 : 1, 2 : 2; 
+
+function searchLetter(s){
+  let convertString = String(s);
+  for(let i = 0; i < convertString.length; i++){
+    let count = 0; 
+    if(convertString[i] === []){
+    count += 1; 
+
+    }
+  }
+}
