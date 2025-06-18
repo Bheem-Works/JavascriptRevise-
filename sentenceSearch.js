6eabb7aae5c8d6cb635fd20f2ccbc30d53350427
bheem-works <shokokomichan098@gmail.com>
2025-06-18 15:39:43 +0545

    codWarsKata6ProblemsSolved

--- sentenceSearch.js
+++ sentenceSearch.js
@@ -2,28 +2,27 @@
 // [a-z] = a:1, b:2; [A-Z] = A:1, B: 2; [1-9] = 1 : 1, 2 : 2; 
 
 function searchLetter(s){
-  let convertString = String(s);
-  let count = 0; // to change it  
-  for(let i = 0; i < convertString.length; i++){
-    // use the regx and the test methods; 
-    if(/[a-z]/.test(convertString[i])){
-    count++;
-    console.log("This is working");
-    console.log(count);
-    } else if(/[A-Z]/.test(convertString[i])) {
-      console.log("This is the seconds if")
-      count +2;
-      console.log(count);
-    } else if(/[0-9]/.test(convertString[i])){
-      console.log("This is the third if statement")
-      count ++;
-      console.log(count);
-    } else {
-      return 0; 
+  // converting input to the string; 
+  const string = String(s);
+  const counts = {}; // for storing the count value; 
+
+  for(let i = 0; i < string.length; i++){
+    const char = string[i];
+    if(/[a-zA-Z0-9]/.test(char)){ // checking all the character match with the regx; 
+      if(!counts[char]){ // If it is not included first then add to it;
+      counts[char] = 1; // Set it to the one; 
+      } else {
+        counts[char]++;
+      }
     }
+  }
+
+  // showing the index numbers; 
+  for(let key in counts) {
+    console.log(`${key}: ${counts[key]}`);
   }
 }
 
-//const setDemo  = ['hellomom']; const setDemo  = ['HELLOMOM']; 
-const setDemo  = ['12345']; 
-searchLetter(setDemo);
+searchLetter("hellomom");
+searchLetter("HELLOMOM");
+searchLetter('12345');
