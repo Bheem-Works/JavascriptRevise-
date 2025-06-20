1c9b383981b9ad0026a60324593e8f3a5a3b4c0a
bheem-works <shokokomichan098@gmail.com>
2025-06-20 22:13:32 +0545

    commit

--- findArray.js
+++ findArray.js
@@ -1,7 +1,10 @@
 function findArray(arr1, arr2) {
   // the first param arr1 is for storing the value;
+  // looping through from the each arrray to get hte param 1 value'
+  for(let i = 0; i < arr1.length; i++){
+    const match = 
+  }
   // the seconds param arr2 is for matching the value; target value;
 }
 
-const moveOne = [0,1,2,3,4];
-console.log(findArray(moveOne));
+console.log(findArray([1,2,1,3,4,1]));
