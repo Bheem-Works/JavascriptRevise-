c76d10db35839f747e7ca12dd7db1a9d921eef61
bheem-works <shokokomichan098@gmail.com>
2025-06-20 21:41:10 +0545

    Push

--- findArray.js
+++ findArray.js
@@ -2,3 +2,6 @@ function findArray(arr1, arr2) {
   // the first param arr1 is for storing the value;
   // the seconds param arr2 is for matching the value; target value;
 }
+
+const moveOne = [0,1,2,3,4];
+console.log(findArray(moveOne));
